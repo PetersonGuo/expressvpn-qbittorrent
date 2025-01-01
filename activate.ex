9b6ddc0c70cb85e77e2093eddba93b887c113477
Peterson Guo <petersonguo@gmail.com>
2024-12-31 21:13:00 -0500

    Take expressvpn docker container

--- activate.ex
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/expect -f
-set timeout -1
-spawn expressvpn activate
-expect "Enter activation code:"
-send "$env(EXPRESSVPN_ACTIVATION_CODE)\r"
-expect "Help improve ExpressVPN: Share crash reports, speed tests, usability diagnostics, and whether VPN connection attempts succeed. These reports never contain personally identifiable information. (Y/n)"
-send "n"
